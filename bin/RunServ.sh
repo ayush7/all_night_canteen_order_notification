07d3b3180b7dc9bc2f56c84dfab3194acdf1d3eb
chawlamit <amitchawla59@gmail.com>
2016-04-17 18:43:08 +0530

    added requirements for uwsgi and bin folder to run uwsgi as http

---
+++ bin/RunServ.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+uwsgi --ini conf.ini
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Run with sudo
+apt-get install python-pip
+pip install webapp2
+pip install jinja2
+pip install webOb
+pip install uwsgi
+
